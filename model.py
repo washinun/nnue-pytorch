5e3f551f013af512e3e2d2b70d3bdc2e659ec50f
washi <washi@DANBO>
2025-08-14 10:54:25 +0900

    ネットの視覚化の修正 R-20くらいになった。

--- model.py
+++ model.py
@@ -14,6 +14,14 @@ L1 = 512
 L2 = 8
 L3 = 64
 
+def coalesce_ft_weights(model, layer):
+  weight = layer.weight.data
+  indices = model.feature_set.get_virtual_to_real_features_gather_indices()
+  weight_coalesced = weight.new_zeros((model.feature_set.num_real_features, weight.shape[1]))
+  for i_real, is_virtual in enumerate(indices):
+    weight_coalesced[i_real, :] = sum(weight[i_virtual, :] for i_virtual in is_virtual)
+  return weight_coalesced
+
 class NNUE(pl.LightningModule):
   """
   This model attempts to directly represent the nodchip Stockfish trainer methodology.
